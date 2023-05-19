92d8b8b185ffdbf3508346957532bf9b4ee3f027
Ekaterina Chehranova <63822020+ktttnv@users.noreply.github.com>
2023-05-19 22:52:08 +0300

    Add React to project (#2)
    * Added React to project

---
+++ src/routes/Home.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+import microphoneIcon from "../images/micro.png";
+
+export const Home = () => {
+    let {push} = useHistory();
+
+    return (
+        <div className="homeContainer">
+            <header className="headerContainer">
+              <h1 className="mainHeading">Audio Recording Transcription</h1>
+            </header>
+
+            <main className="mainContainer">
+              <div className="recordingContainer">
+                <img src={microphoneIcon} alt="Microphone icon" width="48" height="48"/>
+                <div className="time">
+                  <p>00:00</p>
+                </div>
+              </div>
+
+              <div className="buttonContainer">
+                <button className="commonButton">Start</button>
+              </div>
+            </main>
+
+            <footer className="footerContainer">
+                <button onClick={() => {
+                    push('/about')
+                }}>About
+                </button>
+
+                <button onClick={() => {
+                    push('/settings')
+                }}>Settings
+                </button>
+            </footer>
+        </div>
+    )
+}
