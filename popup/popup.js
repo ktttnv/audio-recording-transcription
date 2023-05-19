b699eb13cbcea73f202b82244cfc9a4420ede30b
Sergej Chekhranov <chekhranovsa@bcs.ru>
2023-05-19 21:00:04 +0300

    WIP add recording script

---
+++ popup/popup.js
@@ -0,0 +1,12 @@
+import { initRecording, stopRecording } from '../scripts/recording.js';
+document.querySelector(".commonButton").addEventListener("click", startRecording);
+let counter = 0;
+const storeFolder = "D:\\Projects\\Personal\\TRASH";
+function startRecording() {
+    if (counter % 2 == 0) {
+        initRecording(storeFolder);
+    } else {
+        stopRecording();
+    }
+    counter++;
+}
