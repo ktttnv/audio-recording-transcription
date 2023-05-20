f46de08964e5b9ec55765f87f17528a7c259924f
Ekaterina Chehranova <63822020+ktttnv@users.noreply.github.com>
2023-05-20 14:50:33 +0300

    Improved layout and updated main icon (#4)

--- ui/src/routes/About.tsx
+++ ui/src/routes/About.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { PrimaryLayout } from "../layouts/PrimaryLayout";
 
 export const About = () => {
     let {push} = useHistory();
     return (
-        <div className="aboutContainer">
-            <header>
-                <p>About</p>
-                <button onClick={() => {
-                    push('/')
-                }}>Home page
-                </button>
-            </header>
-        </div>
+      <PrimaryLayout>
+        <p>GitHub: <a href="https://github.com/ktttnv/audio-recording-transcription">link</a></p>
+        <button onClick={() => {
+            push('/')
+        }}>Home page
+        </button>
+      </PrimaryLayout> 
     )
 }
